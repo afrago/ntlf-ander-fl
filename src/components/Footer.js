5ee22dbeec80ddd9a4a3aa2e906704479918ff0b
anderfrago <ander_frago@cuatrovientos.org>
2020-12-25 15:10:57 +0100

    sare sozialak gehituta

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,17 @@ const Footer = () => (
     <div className="inner">
       <ul className="icons">
         <li>
-          <a href="#" className="icon fa-github">
+          <a href="http://github.com/anderfrago" className="icon fa-github">
             <span className="label">Github</span>
           </a>
         </li>
         <li>
-          <a href="#" className="icon fa-linkedin">
+          <a href="http://linkedin.com/in/ander-f-l" className="icon fa-linkedin">
             <span className="label">Linkedin</span>
           </a>
         </li>
         <li>
-          <a href="#" className="icon fa-envelope-o">
+          <a href="mailto:nau_frago@cuatrovientos.org" className="icon fa-envelope-o">
             <span className="label">Email</span>
           </a>
         </li>
